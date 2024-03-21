2ae02edf99d816e6eb9b61b546c57c995a6c61a4
paulomalandrim <paulo.malandrim@gmail.com>
2024-03-21 20:35:58 -0300

    initial commit

---
+++ src/main/java/br/com/malandrim/loansapplication/entity/Customer.java
@@ -0,0 +1,10 @@
+package br.com.malandrim.loansapplication.entity;
+
+public record Customer(
+  int age,
+  String cpf,
+  String name,
+  Double income,
+  String location
+) {
+}
---
+++ src/test/java/br/com/malandrim/loansapplication/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.malandrim.loansapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
